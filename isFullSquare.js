a33ae1c077c89da6b344f08b497853487c806597
Aisse-258 <y-kate@ukr.net>
2019-07-18 12:02:08 +0300

    Faster function

--- isFullSquare.js
+++ isFullSquare.js
@@ -3,19 +3,18 @@
 var sqrt = require ('bigint-isqrt');
 
 function isFullSquare(x){
-    // console.log(x);
-    // Firstly, we divide x by 4 while divisible
-    while ((x & 3n) === 0n && x !== 0n){
-        x >>= 2n;
-    }
-    // A perfect square cannot have residual 2 or 3 modulo 4
-    if ((x & 3n) === 2n || (x & 3n) === 3n){
-        return false;
-    }
-    if ((x & 5n) === 5n){ // x % 7 == 5 or 7
-        return false;
-    }
-    return x === sqrt(x)**2n;
+	// console.log(x);
+	// Firstly, we divide x by 4 while divisible
+	while ((x & 3n) === 0n && x !== 0n){
+		x >>= 2n;
+	}
+	// So, for now x is not divisible by 2
+	// The only possible residual modulo 8 for such x is 1
+ 
+	if ((x & 7n) !== 1n){
+		return false;
+	}
+	return x === sqrt(x)**2n;
 }
 
 module.exports = isFullSquare;
